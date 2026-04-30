0fb30a84d189056b78f8ff6aabaff49d43e2816b
ViiizR <Simonjakobsen1998@hotmail.com>
2026-04-30 19:28:06 +0200

    cube

--- webgl.js
+++ webgl.js
@@ -3,10 +3,15 @@ var gl = document.getElementById('gl')
          document.getElementById('gl')
          .getContext('experimental-webgl');
 
+// Global variable
 var vertices = [];
 var mouseX = 0; var mouseY = 0;
 var angle = [0.0, 0.0, 0.0, 1.0];
 var angleGL = 0;
+var textureGL = 0; // Uniform Location
+var display = [0.0, 0.0, 0.0, 0.0];
+var displayGL = 0; // Uniform Location
+
 
 document.getElementById('gl').addEventListener('mousemove', function (e)
 {
@@ -128,16 +133,21 @@ function InitShaderProgram(vs, fs)
     return p;
 }
 
-function AddVertex(x, y, z, r, g, b)
+function AddVertex(x, y, z, r, g, b, u, v, nx, ny, nz)
 {
     const index = vertices.length;
-    vertices.length += 6;
+    vertices.length += 8;
     vertices[index + 0] = x;
     vertices[index + 1] = y;
     vertices[index + 2] = z;
     vertices[index + 3] = r;
     vertices[index + 4] = g;
     vertices[index + 5] = b;
+    vertices[index + 6] = u;
+    vertices[index + 7] = v;
+    // vertices[index + 8] = nx;
+    // vertices[index + 9] = ny;
+    // vertices[index + 10] = nz;
 }
 
 function CreateGeometryUI()
@@ -146,40 +156,44 @@ function CreateGeometryUI()
     const w = ew ? ew.value : 1.0;
     const eh = document.getElementById('h');
     const h = eh ? eh.value : 1.0;
+    const ed = document.getElementById('d');
+    const d = ed ? ed.value : 1.0;
 
     document.getElementById('ui').innerHTML =
     'Width: <input type="number" id="w" value="' + w + '" onchange="InitShaders();"><br>' +
-    'Height: <input type="number" id="h" value="' + h + '" onchange="InitShaders();">';
+    'Height: <input type="number" id="h" value="' + h + '" onchange="InitShaders();"><br>' +
+    'Depth: <input type="number" id="d" value="' + d + '" onchange="InitShaders();">';
 
     let e = document.getElementById('shape');
     switch (e.selectedIndex)
     {
         case 0: CreateTrinangle(w, h); break;
         case 1: CreateQuad(w, h); break;
+        case 2: CreateCube(w, h, d); break;
     }
 }
 
-function AddTriangle(x1, y1, z1, r1, g1, b1, 
-                     x2, y2, z2, r2, g2, b2,
-                     x3, y3, z3, r3, g3, b3)
+function AddTriangle(x1, y1, z1, r1, g1, b1, u1, v1, 
+                     x2, y2, z2, r2, g2, b2, u2, v2,
+                     x3, y3, z3, r3, g3, b3, u3, v3)
 {
-    AddVertex(x1, y1, z1, r1, g1, b1)
-    AddVertex(x2, y2, z2, r2, g2, b2)
-    AddVertex(x3, y3, z3, r3, g3, b3)
+    AddVertex(x1, y1, z1, r1, g1, b1, u1, v1)
+    AddVertex(x2, y2, z2, r2, g2, b2, u2, v2) 
+    AddVertex(x3, y3, z3, r3, g3, b3, u3, v3)
 }
 
-function AddQuad(x1, y1, z1, r1, g1, b1, 
-                 x2, y2, z2, r2, g2, b2,
-                 x3, y3, z3, r3, g3, b3,
-                 x4, y4, z4, r4, g4, b4)
+function AddQuad(x1, y1, z1, r1, g1, b1, u1, v1, 
+                 x2, y2, z2, r2, g2, b2, u2, v2,
+                 x3, y3, z3, r3, g3, b3, u3, v3,
+                 x4, y4, z4, r4, g4, b4, u4, v4)
 {
-    AddTriangle(x1, y1, z1, r1, g1, b1, 
-                x2, y2, z2, r2, g2, b2,
-                x3, y3, z3, r3, g3, b3);
+    AddTriangle(x1, y1, z1, r1, g1, b1, u1, v1, 
+                x2, y2, z2, r2, g2, b2, u2, v2,
+                x3, y3, z3, r3, g3, b3, u3, v3);
 
-    AddTriangle(x3, y3, z3, r3, g3, b3, 
-                x4, y4, z4, r4, g4, b4,
-                x1, y1, z1, r1, g1, b1);
+    AddTriangle(x3, y3, z3, r3, g3, b3, u3, v3,
+                x4, y4, z4, r4, g4, b4, u4, v4,
+                x1, y1, z1, r1, g1, b1, u1, v1);
 }
 
 function CreateTrinangle(width, height) // funktion der generer koordinater baseret på dimensioner
@@ -187,9 +201,9 @@ function CreateTrinangle(width, height) // funktion der generer koordinater base
     vertices.length = 0;
     const w = width * 0.5;
     const h = height * 0.5;
-    AddTriangle(0.0, h, 0.0, 1.0, 0.0, 0.0,
-                -w, -h, 0.0, 0.0, 1.0, 0.0,
-                 w, -h, 0.0, 0.0, 0.0, 1.0)
+    AddTriangle(0.0, h, 0.0, 1.0, 0.0, 0.0, 0.5, 1.0,
+                -w, -h, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+                 w, -h, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0);
 }
 
 function CreateQuad(width, height) // funktion der generer koordinater baseret på dimensioner
@@ -197,22 +211,70 @@ function CreateQuad(width, height) // funktion der generer koordinater baseret p
     vertices.length = 0;
     const w = width * 0.5;
     const h = height * 0.5;
-    AddQuad (-w,  h, 0.0, 1.0, 0.0, 0.0,
-             -w, -h, 0.0, 0.0, 1.0, 0.0,
-              w, -h, 0.0, 0.0, 0.0, 1.0,
-              w,  h, 0.0, 1.0, 1.0, 0.0)
+    AddQuad (-w,  h, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+             -w, -h, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+              w, -h, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0,
+              w,  h, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0);
 }
+
+function CreateCube(width, height, depth)
+{
+    vertices.length = 0;
+    const w = width * 0.5;
+    const h = height * 0.5;
+    const d = depth * 0.5;
+
+
+
+    //FRONT
+    AddQuad (-w,  h, d, 1.0, 0.0, 0.0, 0.0, 1.0,
+              w,  h, d, 1.0, 0.0, 0.0, 0.0, 0.0,
+              w, -h, d, 1.0, 0.0, 0.0, 1.0, 0.0,
+             -w, -h, d, 1.0, 0.0, 0.0, 1.0, 1.0);
+    //BACK
+    AddQuad (-w,  h, -d, 1.0, 0.0, 0.0, 0.0, 1.0,
+             -w, -h, -d, 0.0, 0.0, 0.0, 0.0, 0.0,
+              w, -h, -d, 0.0, 0.0, 0.0, 1.0, 0.0,
+              w,  h, -d, 1.0, 0.0, 0.0, 1.0, 1.0); 
+    //TOP
+    AddQuad (-w, h, -d, 0.0, 1.0, 0.0, 0.0, 1.0,
+              w, h, -d, 0.0, 1.0, 0.0, 0.0, 0.0,
+              w, h,  d, 0.0, 1.0, 0.0, 1.0, 0.0,
+             -w, h,  d, 0.0, 1.0, 0.0, 1.0, 1.0);
+    //BOTTOM
+    AddQuad (-w, -h,  d, 0.0, 1.0, 0.0, 0.0, 1.0,
+              w, -h,  d, 0.0, 0.0, 0.0, 0.0, 0.0,
+              w, -h, -d, 0.0, 0.0, 0.0, 1.0, 0.0,
+             -w, -h, -d, 0.0, 1.0, 0.0, 1.0, 1.0);
+    //LSIDE
+    AddQuad (-w,  h,  d, 0.0, 0.0, 1.0, 0.0, 1.0,
+             -w, -h,  d, 0.0, 0.0, 1.0, 0.0, 0.0,
+             -w, -h, -d, 0.0, 0.0, 1.0, 1.0, 0.0,
+             -w,  h, -d, 0.0, 1.0, 1.0, 1.0, 1.0);
+    //RSIDE
+    AddQuad (w, -h,  d, 0.0, 0.0, 1.0, 0.0, 1.0,
+             w,  h,  d, 0.0, 0.0, 0.0, 0.0, 0.0,
+             w,  h, -d, 0.0, 0.0, 0.0, 1.0, 0.0,
+             w, -h, -d, 0.0, 0.0, 1.0, 1.0, 1.0);
+    
+} 
 
 function CreateGeometryBuffers(program)
 {
+    // Generate selected geometry and UI
     CreateGeometryUI();
 
-    CreateVBO(program, new Float32Array(vertices))
-
+    // Create GPU buffer (VBO)
+    CreateVBO(program, new Float32Array(vertices));
     angleGL = gl.getUniformLocation(program, 'Angle');
-
+    CreateTexture(program, 'img/texture.jpg');
+    // Activate shader program
     gl.useProgram(program);
-
+    // Update view rotation
+    gl.uniform4fv(angleGL, new Float32Array(angle));
+    // Update display options
+    gl.uniform4fv(displayGL, new Float32Array(display));
+    // Display geometry on screen
     Render();
 } 
 
@@ -222,21 +284,90 @@ function CreateVBO(program, vert)
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.bufferData(gl.ARRAY_BUFFER, vert, gl.DYNAMIC_DRAW);
 
-    const s = 6 * Float32Array.BYTES_PER_ELEMENT;
+    const s = 8 * Float32Array.BYTES_PER_ELEMENT;
 
+    // Create shader attribute: Pos
     let p = gl.getAttribLocation(program, 'Pos');
     gl.vertexAttribPointer(p, 3, gl.FLOAT, gl.FALSE, s, 0);
     gl.enableVertexAttribArray(p);
 
+    // Create shader attribute: Color
     const o = 3 * Float32Array.BYTES_PER_ELEMENT;
     let c = gl.getAttribLocation(program, 'Color');
     gl.vertexAttribPointer(c, 3, gl.FLOAT, gl.FALSE, s, o);
     gl.enableVertexAttribArray(c);
+
+    // Create shader attribute: UV
+    const o2 = o * 2;
+    let u = gl.getAttribLocation(program, 'UV');
+    gl.vertexAttribPointer(u, 2, gl.FLOAT, gl.FALSE, s, o2);
+    gl.enableVertexAttribArray(u);
 }
 
 function Render()
 {
     gl.clearColor(0.0, 0.4, 0.6, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 6);
+    gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 8);
+}
+
+function CreateTexture(prog, url)
+{
+    // Load texture to graphics card
+    const texture = LoadTexture(url);
+    // Flip y axis so it fits OpenGL standard
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    // Activate texture to texture unit 0
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    // Add uniform location to fragment shader
+    textureGL = gl.getUniformLocation(prog, 'Texture');
+    // Add uniform location to fragment shader
+    displayGL = gl.getUniformLocation(prog, 'Display');
+}
+
+function LoadTexture(url)
+{
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    const pixel = new Uint8Array([0, 0, 255, 255]);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixel);
+    const image = new Image();
+    image.onload = () => 
+    {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        SetTextureFilters(image);
+    }
+    image.src = url;
+    return texture;
+}
+
+function IsPow2(value)
+{
+    return (value & (value - 1)) === 0;
+}
+
+function SetTextureFilters(image)
+{
+    if (IsPow2(image.width) && IsPow2(image.height))
+    {
+        gl.generateMipmap(gl.TEXTURE_2D);
+    }
+    else
+    {
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+}
+
+function Update()
+{
+    // Show texture (boolean) last element
+    const t = document.getElementById('t');
+    display[3] = t.checked ? 1.0 : 0.0;
+    // Update array to graphics card and render
+    gl.uniform4fv(displayGL, new Float32Array(display));
+    Render();
 }
